b6cf28b660945b9398d52701fa6ccb4554beb8df
Ghost <ghostridergamer37@gmail.com>
2022-07-04 14:03:16 +0530

    my-nfts page

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import Image from 'next/image';
 
-const MyNFTs = () => (
-  <div>MyNFTs</div>
-);
+import { NFTContext } from '../context/NFTContext';
+import images from '../assets';
+import { NFTCard, Loader, Banner } from '../components';
+import { shortenAddress } from '../utils/shortenAddress';
+
+const MyNFTs = () => {
+  const { fetchMyNFTsOrListedNFTs, currentAccount } = useContext(NFTContext);
+  const [nfts, setNfts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+
+  }, []);
+
+  if (isLoading) {
+    <div className="flexStart min-h-screen">
+      <Loader />
+    </div>;
+  }
+
+  return (
+    <div className="w-full flex justify-start items-center flex-col min-h-screen">
+      <div className="w-full flexCenter flex-col">
+        <Banner
+          name="Your Nifty NFts"
+          childStyles="text-center mt-4"
+          parentStyles="h-80 justify-center"
+        />
+        <div className="flexCenter flex-col z-0 -mt-20">
+          <div className="flexCenter w-40 h-40 sm:w-36 sm:h-36 p-1 bg-nft-black-1 rounded-full">
+            <Image src={images.creator1} className="rounded-full object-cover" objectFit="cover" />
+          </div>
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl mt-6">{shortenAddress(currentAccount)}</p>
+        </div>
+      </div>
+      {!isLoading && !nfts.length ? (
+        <div className="flexCenter p-16 sm:p-4">
+          <h1 className="font-poppins dark:text-white text-nft-black-1 font-extrabold text-3xl">No  NFTs Owned</h1>
+        </div>
+      ) : (
+        <div className="sm:px-4 p-12 w-full mimd:w-4/5 flexCenter flex-col">
+          <div className="flex-1 w-full flex flex-row sm:flex-col px-4 xs:px-0 minlg:px-8">SearchBar</div>
+          <div className="mt-3 w-full flex flex-wrap">
+            {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default MyNFTs;
